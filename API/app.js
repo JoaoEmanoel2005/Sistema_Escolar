86d9da54917ea018f968f36eae84f87363a7564d
Jurimeu <jurimeusoyeu19@gmail.com>
2025-01-01 00:37:44 -0300

    Mensagem explicativa sobre o que foi alterado

--- API/app.js
+++ API/app.js
@@ -10,5 +10,15 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: 'bem vindo a api'})
 })
 
-app.listen(3000)
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.cewwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+
+    app.listen(3000)
+    console.log('conectou ao banco')
+
+}).catch((err) => console.log(err))
+
+
 
