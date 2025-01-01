1c0eba7ae8da97388eacddc3a7037a68679aaccc
Jurimeu <jurimeusoyeu19@gmail.com>
2025-01-01 01:11:10 -0300

    Mensagem explicativa sobre o que foi alterado

--- API/app.js
+++ API/app.js
@@ -6,8 +6,31 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+app.use(express.json())
+
+//models
+const User = require('./models/User')
+
 app.get('/', (req, res) => {
     res.status(200).json({msg: 'bem vindo a api'})
+})
+
+app.post('/auth/register', async(req, res) => {
+
+const {name, email, password, confirmpassword} = req.body
+
+if (!name){
+    return res.status(422).json({msg: 'O nome é obrigatorio' })
+}
+
+if (!email){
+    return res.status(422).json({msg: 'O email é obrigatorio' })
+}
+
+if (!password){
+    return res.status(422).json({msg: 'A password é obrigatorio' })
+}
+
 })
 
 const dbUser = process.env.DB_USER
